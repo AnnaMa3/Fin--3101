99051c7ed294cdfa3329874510ca8817cd26b5e5
AnnaMa3 <ann.matveyenko@gmail.com>
2023-11-27 14:08:49 +0100

    Commit 14 - Post user UPD

--- src/main/java/com/coherent/aqa/java/training/api/matveenko/base/UserHttpClient.java
+++ src/main/java/com/coherent/aqa/java/training/api/matveenko/base/UserHttpClient.java
@@ -1,18 +1,14 @@
 package com.coherent.aqa.java.training.api.matveenko.base;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
-import org.springframework.util.StreamUtils;
-import org.testng.Assert;
+
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Map;
 
 public class UserHttpClient extends BasicHttpClient {
@@ -22,21 +18,6 @@ public class UserHttpClient extends BasicHttpClient {
         this.httpClient = httpClient;
     }
 
-//    public List<String> executeGetZipCodeRequest(String url, String readToken) throws IOException {
-//        HttpGet httpGet = new HttpGet(url);
-//        httpGet.setHeader("Authorization", "Bearer "+ readToken);
-//
-//        CloseableHttpResponse response = (CloseableHttpResponse) httpClient.execute((HttpUriRequest)httpGet);
-//
-//        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200, "Zip Codes is not available");
-//        String responseBody = StreamUtils.copyToString(response.getEntity().getContent(), Charset.defaultCharset());
-//
-//        ObjectMapper entityMapper = new ObjectMapper();
-//        List <String> entity = new ArrayList< >();
-//        entity = entityMapper.readValue(responseBody, List.class);
-//
-//        return entity;
-//    }
 
     public CloseableHttpResponse executePostUserRequest(String url, String writeToken, Map<String, String> map) throws IOException{
         HttpPost httpPost = new HttpPost(url);
@@ -51,13 +32,6 @@ public class UserHttpClient extends BasicHttpClient {
 
         CloseableHttpResponse response = (CloseableHttpResponse) httpClient
                 .execute((HttpUriRequest) httpPost);
-
-//        Assert.assertEquals(response.getStatusLine().getStatusCode(), 201, "User is not added");
-//        String responseBody = StreamUtils.copyToString(response.getEntity().getContent(), Charset.defaultCharset());
-//
-//        ObjectMapper entityMapper = new ObjectMapper();
-//        List <String> entity = new ArrayList< >();
-//        entity = entityMapper.readValue(responseBody, List.class);
 
         return response;
     }
